8364e468f1665ca6c39b8fe98efd522ee6b8f897
Daniel Corcoran <danielc.92@outlook.com>
2019-05-31 13:36:48 +1000

    Added loop example

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 // Create component
 function App() {
 
-
+  const vegetables = ['Lettuce', 'Cucumber', 'Carrots', 'Spinach', 'Potato', 'Celery', 'Beetroot'];
+  
   var counter = 0;
 
   const incrementCounter = () => {
@@ -11,15 +12,19 @@ function App() {
     console.log(counter);
   }
 
-
-
   return (
-    <section>
-      <h1>Title</h1>
-      <p>Lorem</p>
+    <section className="content">
+      <h1>Counters (App.js)</h1>
       <button onClick={incrementCounter}>{counter}</button>
+      <h1>Vegetable list</h1>
+      <ul>
+        {vegetables.map((veggie, index) => {
+          return <li id={index}>{veggie}</li>
+        })}  
+      </ul>        
     </section>
   );
+
 }
 
 export default App;
