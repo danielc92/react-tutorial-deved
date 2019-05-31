6507b552150c6f879f0b86d1537c897b9349cf35
Daniel Corcoran <danielc.92@outlook.com>
2019-05-31 13:58:36 +1000

    Added content to Sources component

---
+++ src/Sources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Sources () {
+    return (
+        <section className="section">
+            <section className="container">
+                <section className="content">
+                    <h1>This is the Sources component</h1>
+                    Tutorial link <a href="https://www.youtube.com/watch?v=dGcsHMXbSOA">here.</a>
+                </section>
+            </section>
+        </section>
+    );
+}
