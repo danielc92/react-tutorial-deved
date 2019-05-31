10655c9ee33cb1f6d8a23ced8af97f55ff88148e
Daniel Corcoran <danielc.92@outlook.com>
2019-05-31 14:55:26 +1000

    Implemented useState function from 'react'

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import React from 'react';
 import Contact from './Contact';
 import Sources from './Sources';
 import Message from './Message';
+import {useState} from 'react';
 
 // Create component
 function App() {
 
   // Hardcode data in for now
   const vegetables = ['Lettuce', 'Cucumber', 'Carrots', 'Spinach', 'Potato', 'Celery', 'Beetroot'];
-  const messages = [{'author':'Harry', 'message':'Who is there?', 'date':'2013-2-10'}, 
-  {'author':'Johnny', 'message':'Bye bye.', 'date':'2019-2-10'}, 
-  {'author':'Cole', 'message':'How are you?', 'date':'2019-5-20'}
-];
+  const messages = [{'author':'Harry', 'message':'Who is there?', 'date':'2013-2-10'}, {'author':'Johnny', 'message':'Bye bye.', 'date':'2019-2-10'}, {'author':'Cole', 'message':'How are you?', 'date':'2019-5-20'}];
   
+  // Normally api is called is get this state value.
+  const [likes, setLikes] = useState(0); 
+
+  const incrementLikes = () => {
+    setLikes(likes + 1);
+  };
+
   var counter = 0;
 
   // Counter function for buton
@@ -28,8 +33,10 @@ function App() {
             <section className="content">
               <h1>Main component</h1>
               <h3>Counters (App.js)</h3>
-              <button id="counter" onClick={incrementCounter}>Click Me</button>
-
+              <button className="button is-outlined is-primary" id="counter" onClick={incrementCounter}>Click Me</button>
+              <h3>Likes (useState)</h3>
+              <button className="button is-outlined is-primary" onClick={incrementLikes}>Change state value of likes</button>
+              <p>Current value of likes: {likes}</p>
               <h3>Vegetable list</h3>
               <ul>
                 {vegetables.map((veggie, index) => {
